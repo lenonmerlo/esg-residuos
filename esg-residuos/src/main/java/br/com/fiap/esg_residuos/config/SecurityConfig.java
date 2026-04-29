5814a1862772e191b22ea6dcdad002607b27b268
lenonmerlo <lenontm@gmail.com>
2026-04-29 11:58:41 -0300

    feat: add swagger and security config

---
+++ esg-residuos/src/main/java/br/com/fiap/esg_residuos/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.fiap.esg_residuos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
